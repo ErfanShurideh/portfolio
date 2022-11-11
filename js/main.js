fde649a644fe327c9dce34634410fa08bca2fd68
ErfanShurideh <erfan.shurideh@gmail.com>
2022-11-12 01:56:09 +0330

    Talk to me

--- js/main.js
+++ js/main.js
@@ -42,3 +42,11 @@ let swiperProjects = new Swiper(".projects__container", {
       },
   });
 
+    /*----SWIPER TESTIMONIAL----*/   
+    let swiperTestimonial = new Swiper(".testimonial__container", {
+        grabCursor : true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
