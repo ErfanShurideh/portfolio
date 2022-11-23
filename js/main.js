d7e3ebec8287c8818436351354d107270bf21a50
ErfanShurideh <erfan.shurideh@gmail.com>
2022-11-23 19:18:20 +0330

    footer and scrollbar

--- js/main.js
+++ js/main.js
@@ -91,9 +91,36 @@ let swiperProjects = new Swiper(".projects__container", {
           contactMessage.textContent = err.text;
         })
 
+        //to clear input field
+        contactName.value = ''
+        contactEmail.value = ''
+        contactProject.value = ''
+
       }
     }      
 
     console.log(contactForm);
     
     contactForm.addEventListener('submit', sendEmail)
+
+
+    /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
